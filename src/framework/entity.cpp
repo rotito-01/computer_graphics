e953abda98c1bac4a7a575da9bd60188e79807b6
Isa Oliver <isabelmaria.oliver01@estudiant.upf.edu>
2026-01-27 19:51:44 +0100

    IGNORE!! wireframe attempt

--- src/framework/entity.cpp
+++ src/framework/entity.cpp
@@ -12,34 +12,88 @@ Entity::Entity(Mesh* m, Matrix44 mm) {
 
 void Entity::Render(Image* framebuffer, Camera* camera, const Color& c) {
 	
-	Vector4 cur_vec;
-	Vector4 proj_v;
-	for (int i = 0; i < mesh->GetVertices().size(); i++) {
-		cur_vec.x = mesh->GetVertices()[i].x;
-		cur_vec.y = mesh->GetVertices()[i].y;
-		cur_vec.z = mesh->GetVertices()[i].z;
-		cur_vec.w = 1;
-		proj_v = model_matrix * cur_vec; // world coordinates
+	Vector4 cur_vec1;
+    Vector4 cur_vec2;
+    Vector4 cur_vec3;
+
+	Vector4 proj_v1;
+    Vector4 proj_v2;
+    Vector4 proj_v3;
+
+    Vector2 v1;
+    Vector2 v2;
+    Vector2 v3;
+
+	for (int i = 0; i < mesh->GetVertices().size(); i=i+3) {
+		cur_vec1.x = mesh->GetVertices()[i].x;
+		cur_vec1.y = mesh->GetVertices()[i].y;
+		cur_vec1.z = mesh->GetVertices()[i].z;
+		cur_vec1.w = 1;
+		proj_v1 = model_matrix * cur_vec1; // world coordinates
         
-        proj_v = camera->GetViewProjectionMatrix() * proj_v; // View Matrix and Projection Matrix all in one
+        cur_vec2.x = mesh->GetVertices()[i + 1].x;
+        cur_vec2.y = mesh->GetVertices()[i + 1].y;
+        cur_vec2.z = mesh->GetVertices()[i + 1].z;
+        cur_vec2.w = 1;
+        proj_v2 = model_matrix * cur_vec2;
+
+        cur_vec3.x = mesh->GetVertices()[i + 2].x;
+        cur_vec3.y = mesh->GetVertices()[i + 2].y;
+        cur_vec3.z = mesh->GetVertices()[i + 2].z;
+        cur_vec3.w = 1;
+        proj_v3 = model_matrix * cur_vec3;
+
+        proj_v1 = camera->GetViewProjectionMatrix() * proj_v1; // View Matrix and Projection Matrix all in one
+        proj_v2 = camera->GetViewProjectionMatrix() * proj_v2;
+        proj_v3 = camera->GetViewProjectionMatrix() * proj_v3;
         // Clamp already done ..?
         
 		// camera project pv
 		// clamp [-1, 1]
-        proj_v.x = proj_v.x / proj_v.w;
-        proj_v.y = proj_v.y / proj_v.w;
-        proj_v.z = proj_v.z / proj_v.w;
+        proj_v1.x = proj_v1.x / proj_v1.w;
+        proj_v1.y = proj_v1.y / proj_v1.w;
+        proj_v1.z = proj_v1.z / proj_v1.w;
+
+        proj_v2.x = proj_v2.x / proj_v2.w;
+        proj_v2.y = proj_v2.y / proj_v2.w;
+        proj_v2.z = proj_v2.z / proj_v2.w;
+
+        proj_v3.x = proj_v3.x / proj_v3.w;
+        proj_v3.y = proj_v3.y / proj_v3.w;
+        proj_v3.z = proj_v3.z / proj_v3.w;
+
+        if ((proj_v1.x < -1 || proj_v1.x > 1) || (proj_v1.y < -1 || proj_v1.y > 1) || (proj_v1.z < -1 || proj_v1.z > 1) ||
+            (proj_v2.x < -1 || proj_v2.x > 1) || (proj_v2.y < -1 || proj_v2.y > 1) || (proj_v2.z < -1 || proj_v2.z > 1) ||
+            (proj_v3.x < -1 || proj_v3.x > 1) || (proj_v3.y < -1 || proj_v3.y > 1) || (proj_v3.z < -1 || proj_v3.z > 1)) {
+            continue;
+        }
         
 		// viewport [-1,1] -> [0, w]
         
-        proj_v.x = (proj_v.x - -1) / (1 - (-1));
-        proj_v.y = (proj_v.y - -1) / (1 - (-1));
-        proj_v.z = (proj_v.z - -1) / (1 - (-1));
+        proj_v1.x = (proj_v1.x - -1) / (1 - (-1));
+        proj_v1.y = (proj_v1.y - -1) / (1 - (-1));
+        proj_v1.z = (proj_v1.z - -1) / (1 - (-1));
+
+        proj_v2.x = (proj_v2.x - -1) / (1 - (-1));
+        proj_v2.y = (proj_v2.y - -1) / (1 - (-1));
+        proj_v2.z = (proj_v2.z - -1) / (1 - (-1));
+
+        proj_v3.x = (proj_v3.x - -1) / (1 - (-1));
+        proj_v3.y = (proj_v3.y - -1) / (1 - (-1));
+        proj_v3.z = (proj_v3.z - -1) / (1 - (-1));
         
-        proj_v.x = proj_v.x * framebuffer->width;
-        proj_v.y = proj_v.y * framebuffer->height;
+        v1.x = proj_v1.x * framebuffer->width;
+        v1.y = proj_v1.y * framebuffer->height;
+
+        v2.x = proj_v2.x * framebuffer->width;
+        v2.y = proj_v2.y * framebuffer->height;
+
+        v3.x = proj_v3.x * framebuffer->width;
+        v3.y = proj_v3.y * framebuffer->height;
+        
+
         
 		//framebuffer Set Pixel
-        framebuffer->SetPixel(int(proj_v.x), int(proj_v.y), c);
+        framebuffer->DrawTriangle(v1, v2, v3, c, true, c);
 	}
 }
