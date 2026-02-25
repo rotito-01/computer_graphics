e22860c199882077828f147a9e4f06d92a840ac7
rotito-01 <adan.pr@proton.me>
2026-02-25 18:33:20 +0100

    hello isa

--- res/shaders/quad.fs
+++ res/shaders/quad.fs
@@ -2,69 +2,81 @@ varying vec2 v_uv;
 uniform int u_task;
 uniform int u_subtask;
 uniform sampler2D u_texture;
-uniform vec2 u_res;
+
 
 void main()
 {
-	vec2 uv = v_uv.xy/u_res.xy;
-	vec3 color = vec3(1.0, 0.0, 0.0);
-	vec4 texture_color;
-	float pi = 3.14159265359;
 
-	if (u_task == 1){
-// Drawing formulas
-		if (u_subtask == 1){
-		// 1 a
-			color = vec3(uv.x, 0.0, 1-uv.x);
-		} else if (u_subtask == 2){
-		// 1 b
-			vec2 center = vec2(0.5,0.5);
-			color = vec3(distance(uv, center));
-		} else if (u_subtask == 3){
-		// 1 c
-            float r = sin(8.0 * pi * uv.x);
-			float g = 0;
-            float b = sin(8.0 * pi * uv.y);
-            color = vec3(r, g, b);
-		} else if (u_subtask == 4){
-		// 1 d
-			float x = floor(uv.x*20);
-			float y = floor(uv.y*20);
-			color = vec3((x/20) ,(y/20) , 0.0);
-		} else if (u_subtask == 5){
-		// 1 e
-			float sum = floor(uv.x * 16) + floor(uv.y * 16);
-			color = vec3(mod(sum,2.0), mod(sum,2.0), mod(sum,2.0));
-		} else if (u_subtask == 6){
-		// 1 f
-			float wave = 0.5 + 0.25 *sin(uv.x * 2.0 * pi);
-			float step_up = step(wave, uv.y);
-			float step_down = 1.0 - step_up;
-			float gradient = (1 - uv.y) * step_up + uv.y * step_down;
-			color = vec3(0.0, gradient, 0.0);
-		} else if (u_subtask == 7){
-		// EXTRA
-			uv.x *= u_res.x/u_res.y;
-			vec3 color = vec3(0.0);
-			float d = 0.0;
+vec3 color = vec3(1.0, 0.0, 0.0);
+vec4 texture_color;
+float pi = 3.14159265359;
 
-			// Remap the space to -1. to 1.
-			uv = uv *2.-1.;
+if (u_task == 1){
+    // Drawing formulas
+    if (u_subtask == 1){
+        // 1 a
+        color = vec3(v_uv.x, 0.0, 1.0-v_uv.x);
+    } else if (u_subtask == 2){
+        // 1 b
+        vec2 center = vec2(0.5,0.5);
+        color = vec3(distance(v_uv, center));
+    } else if (u_subtask == 3){
+        // 1 c
+        vec2 st = gl_FragCoord.xy / vec2(640, 480);
+        float r = sin(8.0 * 3.14159265359 * st.x);
+        float g = 0.0;
+        float b = sin(8.0 * 3.14159265359 * st.y);
+        color = vec3(r, g, b);
+    } else if (u_subtask == 4){
+        // 1 d
+        float x = floor(v_uv.x*20.0);
+        float y = floor(v_uv.y*20.0);
+        color = vec3((x/20.0) ,(y/20.0) , 0.0);
+    } else if (u_subtask == 5){
+        // 1 e
+        float sum = floor(v_uv.x * 16.0) + floor(v_uv.y * 16.0);
+        color = vec3(mod(sum,2.0), mod(sum,2.0), mod(sum,2.0));
+    } else if (u_subtask == 6){
+    // 1 f
+        float wave = 0.5 + 0.25 *sin(v_uv.x * 2.0 * pi);
+        float step_up = step(wave, v_uv.y);
+        float step_down = 1.0 - step_up;
+        float gradient = (1.0 - v_uv.y) * step_up + v_uv.y * step_down;
+        color = vec3(0.0, gradient, 0.0);
+    }
+    gl_FragColor = vec4(color, 1.0);
+} else if (u_task == 2){
+    // Image filters
+    texture_color = texture2D(u_texture, v_uv);
+    if (u_subtask == 1){
+    // 2 a
+        float med = (texture_color.x+texture_color.y+texture_color.z) / 3.0;
+        texture_color = vec4(med,med,med,1.0);
+    } else if (u_subtask == 2) {
+        texture_color = vec4(1.0-texture_color.x, 1.0-texture_color.y, 1.0-texture_color.z, 1.0);
+    } else if (u_subtask == 3) {
+        float med2 = (texture_color.x+texture_color.y) / 2.0;
+        texture_color = vec4(med2, med2, 0.0, 1.0);
+    } else if (u_subtask == 4) {
+        float sum = texture_color.x+texture_color.y+texture_color.z;
+        float sum2 = sum / 1.5;
+        texture_color = vec4(floor(sum2), floor(sum2), floor(sum2), 1.0);
+    } else if (u_subtask == 5) {
+        // 1 b
+        
+        
+        
+        float intensityx = distance(texture_color.x, 0.5);
+        float intensityy = distance(texture_color.y, 0.5);
+        float intensityz = distance(texture_color.z, 0.5);
+        float intensityw = distance(texture_color.w, 0.5);
 
-			// Make the distance field
-			d = length( abs(uv)-.3 );
+        
+        texture_color = vec4(texture_color.x, texture_color.y, texture_color.z, intensityw);
+    }
 
-			color = vec3(fract(d*10.0));
-		}
-		gl_FragColor = vec4(color, 1.0);
-	} else if (u_task == 2){
-// Image filters
-		if (u_subtask == 1){
-		// 2 a
-			texture_color = texture2D(u_texture, v_uv);
-		}
-		gl_FragColor = texture_color;
-	}
-	
-	
+
+    gl_FragColor = texture_color;
+}
+
 }
